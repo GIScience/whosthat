fd786b908373545bdfd0346d46be3113b7d78c77
Johannes Visintini <johannes.visintini@heigit.org>
2026-05-08 22:19:40 +0200

    scripts: update run in verbose mode

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/opt/parse_osc.pl -h ${DB_HOST} -o ${DB_PORT} -d ${DB_NAME} -u ${DB_USER} -p ${DB_PASSWORD} -l ${REPLICATION_URL}
+/opt/parse_osc.pl -h ${DB_HOST} -o ${DB_PORT} -d ${DB_NAME} -u ${DB_USER} -p ${DB_PASSWORD} -l ${REPLICATION_URL} -v
